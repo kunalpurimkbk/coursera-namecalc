4a2476f3ed7e472fb89ef407e7cb5aa4daf7a251
Testing <testing@example.com>
2018-09-16 12:59:43 +0530

    Restructure code using Angular

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-function onInputDone() {
-    var str = document.getElementById("name").value;
-    var codeValue = calculateCodeValue(str);
-    document.getElementById("nameCodeValue").innerHTML = "The code value is: " + codeValue;
-}
-
-function calculateCodeValue(str) {
-    var codeValue = 0;
-
-    for (var i = 0; i < str.length; i++) {
-        codeValue += str.charCodeAt(i);
+(
+    function() {
+        "use strict;"
+        angular.module("nameCalcApp", [])
+                .controller("nameDivision", function($scope) {
+            $scope.name = "";
+            $scope.getCodeFromName = function(str) {
+                var codeValue = 0;
+            
+                for (var i = 0; i < str.length; i++) {
+                    codeValue += str.charCodeAt(i);
+                }
+            
+                return "" + codeValue;
+            }
+        });
     }
-
-    return codeValue;
-}
-
-function onPageLoaded() {
-    onInputDone();
-}
+)();
